cde21c9762d614ee340e82da3451b73e79385cbf
Abhishek Choudhury <abhishek.choudhury2697@gmail.com>
2024-02-10 16:39:42 +0530

    solve anagram

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -4,8 +4,20 @@
   - A word, phrase, or name formed by rearranging the letters of another, such as spar, formed from rasp.
 */
 
-function isAnagram(str1, str2) {
-
-}
+const isAnagram = (str1, str2) => {
+  if (typeof str1 !== "string" || typeof str2 !== "string") return false;
+  if (str1.length !== str2.length) return false;
+  const charCounts = new Map();
+  for (let char of str1.toLowerCase()) {
+    const count = charCounts.get(char) || 0;
+    charCounts.set(char, count + 1);
+  }
+  for (let char of str2.toLowerCase()) {
+    const count = charCounts.get(char);
+    if (count === undefined || count <= 0) return false;
+    charCounts.set(char, count - 1);
+  }
+  return true;
+};
 
 module.exports = isAnagram;
