16ae8d468cb1ed99f24aeccf59f2f8c9c5cb29cf
Abhishek Choudhury <abhishek.choudhury2697@gmail.com>
2024-02-11 01:31:23 +0530

    solve largest number

--- 01-js/easy/findLargestElement.js
+++ 01-js/easy/findLargestElement.js
@@ -6,7 +6,10 @@
 */
 
 function findLargestElement(numbers) {
-    
+  if (!Array.isArray(numbers) || numbers.length === 0) return undefined;
+  return numbers.reduce((accumulator, number) => {
+    return accumulator > number ? accumulator : number;
+  }, -Infinity);
 }
 
 module.exports = findLargestElement;
